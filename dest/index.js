64fefe3818dce98526ff6b083127e9836deeeba8
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-05-08 02:16:59 +0300

    Added posts-router.ts

--- dest/index.js
+++ dest/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.STATUSES_HTTP = void 0;
 const express_1 = __importDefault(require("express"));
 const blogs_router_1 = require("./routes/blogs-router");
+const posts_router_1 = require("./routes/posts-router");
 exports.STATUSES_HTTP = {
     OK_200: 200,
     CREATED_201: 201,
@@ -18,6 +19,7 @@ const port = 7050;
 const jsonBodyMW = express_1.default.json();
 app.use(jsonBodyMW);
 app.use('/blogs', blogs_router_1.blogsRouter);
+app.use('/posts', posts_router_1.postsRouter);
 app.delete('/testing/all-data', (req, res) => {
     blogs_router_1.db_blogs.blogs = [];
     res.sendStatus(exports.STATUSES_HTTP.NO_CONTENT_204);
