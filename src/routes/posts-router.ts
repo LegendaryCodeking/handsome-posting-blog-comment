41f071e798f8e398c072c769cebbc4f586066a4b
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-05-08 04:26:22 +0300

    Updated blogID validation wo check the blog with such id exists

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -42,7 +42,7 @@ const authorizationCheck = (req: Request, res: Response, next: NextFunction) => 
 const titleValidation = body("title").isString().withMessage("Title should be string").trim().isLength({min: 1, max: 15}).withMessage("The length should be from 1 to 15 symbols")
 const shortDescription = body("shortDescription").isString().withMessage("shortDescription should be string").trim().isLength({min: 1, max: 100}).withMessage("The length should be from 1 to 100 symbols")
 const content = body("content").isString().withMessage("content should be string").trim().isLength({min: 1, max: 1000}).withMessage("The length should be from 1 to 1000 symbols")
-const blogId = body("blogId").isString().withMessage("blogId should be string").trim().isLength({min: 1}).withMessage("The length should be > 0")
+const blogId = body("blogId").isString().withMessage("blogId should be string").trim().isLength({min: 1}).withMessage("The length should be > 0").isIn(db_posts.posts.map(value => value.id)).withMessage("There is no blog with such ID")
 const inputValidationMw = (req: Request, res: Response, next: NextFunction) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
