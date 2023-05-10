6c528f24216c9d2b7b381d5431666340607570c3
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-05-10 15:28:58 +0300

    Added tests for posting blogs without auth and with one + correct data

--- src/__tests__/e2e/hw2.api.test.ts
+++ src/__tests__/e2e/hw2.api.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
 import {app} from "../../index";
+import {STATUSES_HTTP} from "../../routes/http-statuses-const";
+import * as string_decoder from "string_decoder";
 
 describe('/blogs', () => {
     beforeAll(async () => {
@@ -10,15 +12,50 @@ describe('/blogs', () => {
     it('should return 404 and empty array', async () => {
         await request(app)
             .get('/blogs')
-            .expect(404, [])
+            .expect(STATUSES_HTTP.NOT_FOUND_404, [])
     })
 
     it('should return 404 for not existing blog', async () => {
         await request(app)
             .get('/blogs/22222222220')
-            .expect(404)
+            .expect(STATUSES_HTTP.NOT_FOUND_404)
     })
 
+    it('should not create blog without AUTH', async () => {
+        await request(app)
+            .post('/blogs')
+            .send({
+                "name": "Richard Feynman",
+                "description": "Bingo article about Richard Feynman",
+                "websiteUrl": "https://telegra.ph/Richard-Feynman-05-11"
+            })
+            .expect(STATUSES_HTTP.UNAUTHORIZED_401)
+
+        await request(app)
+            .get('/blogs')
+            .expect(STATUSES_HTTP.NOT_FOUND_404, [])
+    })
+
+    it('should create blog with AUTH and correct input data', async () => {
+        let readyResponse = await request(app)
+            .post('/blogs')
+            .set({Authorization: "Basic YWRtaW46cXdlcnR5"})
+            .send({
+                "name": "Richard Feynman",
+                "description": "Bingo article about Richard Feynman",
+                "websiteUrl": "https://telegra.ph/Richard-Feynman-05-11"
+            })
+            .expect(STATUSES_HTTP.CREATED_201)
+
+        const createdBlog = readyResponse.body
+
+        expect(createdBlog).toEqual({
+            "id": expect.any(String),
+            "name": "Richard Feynman",
+            "description": "Bingo article about Richard Feynman",
+            "websiteUrl": "https://telegra.ph/Richard-Feynman-05-11"
+        })
+    })
 
 
     // it('', () => {
