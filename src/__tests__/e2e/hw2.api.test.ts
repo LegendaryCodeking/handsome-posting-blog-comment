9abf14c51bdc73dab3fe06186290faaf859173a3
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-05-10 16:50:04 +0300

    Added some tests for Blogs PUT requests

--- src/__tests__/e2e/hw2.api.test.ts
+++ src/__tests__/e2e/hw2.api.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import {app} from "../../index";
 import {STATUSES_HTTP} from "../../routes/http-statuses-const";
 import * as string_decoder from "string_decoder";
+import {BlogType} from "../../models/BlogModel";
 
 describe('/blogs', () => {
     beforeAll(async () => {
@@ -36,6 +37,17 @@ describe('/blogs', () => {
             .expect(STATUSES_HTTP.NOT_FOUND_404, [])
     })
 
+    /*
+    * Created variable outside the next test to have option use
+    * id of created blog in the further put test
+    * */
+    let createdBlog: BlogType = {
+        "id": "",
+        "name": "",
+        "description": "",
+        "websiteUrl": ""
+    };
+
     it('should create blog with AUTH and correct input data', async () => {
         let readyResponse = await request(app)
             .post('/blogs')
@@ -47,7 +59,7 @@ describe('/blogs', () => {
             })
             .expect(STATUSES_HTTP.CREATED_201)
 
-        const createdBlog = readyResponse.body
+        createdBlog = readyResponse.body
 
         expect(createdBlog).toEqual({
             "id": expect.any(String),
@@ -61,26 +73,75 @@ describe('/blogs', () => {
             .expect(STATUSES_HTTP.OK_200, [createdBlog])
     })
 
+    it('should not update blog with AUTH and incorrect input data', async () => {
+        await request(app)
+            .put(`/blogs/${createdBlog.id}`)
+            .set({Authorization: "Basic YWRtaW46cXdlcnR5"})
+            .send({
+                "name": "",
+                "description": "Bingo article about Richard Feynman 2222",
+                "websiteUrl": "https://telegra.ph/Richard-Fey2222nman-05-11"
+            })
+            .expect(STATUSES_HTTP.BAD_REQUEST_400)
 
-    // it('', () => {
-    //
-    // })
-    // it('', () => {
-    //
-    // })
-    // it('', () => {
-    //
-    // })
-    // it('', () => {
-    //
-    // })
-    // it('', () => {
-    //
-    // })
-    // it('', () => {
-    //
-    // })
-    // it('', () => {
-    //
-    // })
+
+        await request(app)
+            .get(`/blogs/${createdBlog.id}`)
+            .expect(STATUSES_HTTP.OK_200, createdBlog)
+    })
+
+    it('should update blog with AUTH and correct input data', async () => {
+        await request(app)
+            .put(`/blogs/${createdBlog.id}`)
+            .set({Authorization: "Basic YWRtaW46cXdlcnR5"})
+            .send({
+                "name": "Richard Feynman",
+                "description": "Bingo article about Richard Feynman 2222",
+                "websiteUrl": "https://telegra.ph/Richard-Fey2222nman-05-11"
+            })
+            .expect(STATUSES_HTTP.NO_CONTENT_204)
+
+
+        await request(app)
+            .get(`/blogs/${createdBlog.id}`)
+            .expect(STATUSES_HTTP.OK_200, {
+                ...createdBlog,
+                "description": "Bingo article about Richard Feynman 2222",
+                "websiteUrl": "https://telegra.ph/Richard-Fey2222nman-05-11"
+            })
+    })
+
+    it('should not update blog without AUTH and correct input data', async () => {
+        await request(app)
+            .put(`/blogs/${createdBlog.id}`)
+            .send({
+                "name": "Richard Feynman",
+                "description": "Bingo article about Richard Feynman 33333",
+                "websiteUrl": "https://telegra.ph/Richard-Fey33333nman-05-11"
+            })
+            .expect(STATUSES_HTTP.UNAUTHORIZED_401)
+
+
+        await request(app)
+            .get(`/blogs/${createdBlog.id}`)
+            .expect(STATUSES_HTTP.OK_200, {
+                ...createdBlog,
+                "description": "Bingo article about Richard Feynman 2222",
+                "websiteUrl": "https://telegra.ph/Richard-Fey2222nman-05-11"
+            })
+    })
+
+    it('should not update blog with AUTH and nonexistent шв ', async () => {
+        await request(app)
+            .put(`/blogs/404`)
+            .set({Authorization: "Basic YWRtaW46cXdlcnR5"})
+            .send({
+                "name": "Richard Feynman",
+                "description": "Bingo article about Richard Feynman 2222",
+                "websiteUrl": "https://telegra.ph/Richard-Fey2222nman-05-11"
+            })
+            .expect(STATUSES_HTTP.NOT_FOUND_404)
+    })
+
+
 })
