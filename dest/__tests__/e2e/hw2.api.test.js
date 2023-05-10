d77fd5eee4588ace86302e950f012b5e886b47c0
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-05-10 14:44:49 +0300

    Added initial files for e2e tests

---
+++ dest/__tests__/e2e/hw2.api.test.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_test_1 = require("node:test");
+(0, node_test_1.describe)('/blogs', () => {
+    it('', () => {
+    });
+    it('', () => {
+    });
+    it('', () => {
+    });
+    it('', () => {
+    });
+    it('', () => {
+    });
+    it('', () => {
+    });
+    it('', () => {
+    });
+    it('', () => {
+    });
+});
