47f5c61a7bee33023a0fbeccc1dde5a8a6301506
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-05-23 22:00:51 +0300

    Additional sort for post GET with pagination 4

--- src/repos/posts-repo.ts
+++ src/repos/posts-repo.ts
@@ -22,7 +22,7 @@ export const postsRepo = {
 
         let foundPosts = await postsCollection
             .find(findFilter)
-            .sort((queryFilter.sortBy === 'createdAt' ? {[queryFilter.sortBy]: (queryFilter.sortDirection === 'asc' ? 1 : -1)} : {[queryFilter.sortBy]: (queryFilter.sortDirection === 'asc' ? 1 : -1)}))
+            .sort((queryFilter.sortBy === 'createdAt' ? {[queryFilter.sortBy]: (queryFilter.sortDirection === 'asc' ? 1 : -1)} : {[queryFilter.sortBy]: (queryFilter.sortDirection === 'asc' ? 1 : -1), 'createdAt': 1}))
             .skip((queryFilter.pageNumber - 1) * queryFilter.pageSize)
             .limit(queryFilter.pageSize)
             .map(post => getPostViewModel(post)).toArray();
