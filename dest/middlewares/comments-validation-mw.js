04a43c760f1fc1efd56e3ab2a1d7a02f81cc32db
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-15 18:10:31 +0300

    Delete Dest folder from git

--- dest/middlewares/comments-validation-mw.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contentValidation = void 0;
-const express_validator_1 = require("express-validator");
-exports.contentValidation = (0, express_validator_1.body)("content")
-    .isString().withMessage(`Content field should be string type`)
-    .trim()
-    .isLength({ min: 20, max: 300 }).withMessage(`Comment's length should be from 20 to 300 symbols`);
--- dest/types/posts-types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export type RequestWithBody<T> = Request<{},{},T>
-// export type RequestWithQuery<T> = Request<{},{},{},T>
-// export type RequestWithParamsAndBody<T,B> = Request<T,{},B>
