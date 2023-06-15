2ee1901d68bee3154c05723d0abf2d58a2a19359
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-15 18:56:20 +0300

    Initial blogsController

---
+++ src/controller/blogs-controller.ts
@@ -0,0 +1,41 @@
+import {BlogType} from "../models/BlogModel";
+import {blogsRepo} from "../repos/blogs-repo";
+import {BlogPostFilterModel} from "../models/FilterModel";
+import {BlogsWithPaginationModel} from "../models/BlogsWithPaginationModel";
+import {postsRepo} from "../repos/posts-repo";
+
+
+export const blogsController = {
+    async findBlogs(queryFilter: BlogPostFilterModel): Promise<BlogsWithPaginationModel> {
+        return blogsRepo.findBlogs(queryFilter);
+    },
+    async findBlogById(id: string): Promise<BlogType | null> {
+        return blogsRepo.findBlogById(id)
+    },
+    async deleteBlog(id: string): Promise<boolean> {
+        return blogsRepo.deleteBlog(id)
+    },
+    async createBlog(name: string, description: string, websiteUrl: string): Promise<BlogType> {
+        const createdBlog = {
+            "id": (+(new Date())).toString(),
+            "name": name,
+            "description": description,
+            "websiteUrl": websiteUrl,
+            "createdAt": new Date().toISOString(),
+            "isMembership": false
+        }
+
+        return await blogsRepo.createBlog(createdBlog)
+
+    },
+    async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
+        return blogsRepo.updateBlog(id, name, description, websiteUrl)
+    },
+    async deleteAll() {
+        await blogsRepo.deleteAll();
+
+    },
+    async findPostsByBlogId(queryFilter: BlogPostFilterModel) {
+        return postsRepo.findPosts(queryFilter)
+    }
+}
