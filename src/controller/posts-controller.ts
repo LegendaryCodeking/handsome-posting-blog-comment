9a1c06e1ad0a565c4c0a7cb113bd2fd0fed76aae
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-16 04:57:52 +0300

    Post-controller initial draft

---
+++ src/controller/posts-controller.ts
@@ -0,0 +1,37 @@
+import {PostType} from "../models/PostModel";
+import {postsRepo} from "../repos/posts-repo";
+import {PostsWithPaginationModel} from "../models/PostsWithPaginationModel";
+import {BlogPostFilterModel} from "../models/FilterModel";
+
+export const postsService = {
+    async findPosts(queryFilter: BlogPostFilterModel): Promise<PostsWithPaginationModel> {
+        return postsRepo.findPosts(queryFilter)
+    },
+    async findProductById(id: string): Promise<PostType | null> {
+        return postsRepo.findPostsById(id)
+    },
+    async deletePost(id: string): Promise<boolean> {
+        return postsRepo.deletePost(id)
+    },
+    async createPost(title: string, shortDescription: string, content: string, blogId: string): Promise<PostType> {
+        const createdPost = {
+            "id": (+(new Date())).toString(),
+            "title": title,
+            "shortDescription": shortDescription,
+            "content": content,
+            "blogId": blogId,
+            "blogName": "BlogName",
+            "createdAt": new Date().toISOString(),
+            "comments": []
+
+        };
+
+        return await postsRepo.createPost(createdPost)
+    },
+    async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean> {
+        return postsRepo.updatePost(id,title,shortDescription,content,blogId)
+    },
+    async deleteAll() {
+        await postsRepo.deleteAll()
+    }
+}
