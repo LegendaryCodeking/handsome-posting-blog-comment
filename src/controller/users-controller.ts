4a373915459d0bc32e2f9836f24d81851c21fce6
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-16 05:17:43 +0300

    User-controller main commit

---
+++ src/controller/users-controller.ts
@@ -0,0 +1,33 @@
+import {queryBlogPostPagination} from "../models/FilterModel";
+import {UsersWithPaginationModel} from "../models/UsersWithPaginationModel";
+import {Request, Response} from "express";
+import {STATUSES_HTTP} from "../routes/http-statuses-const";
+import {userService} from "../domain/user-service";
+
+export const usersController = {
+    async findAllUsers(req: Request, res: Response<UsersWithPaginationModel>) {
+        let queryFilter = queryBlogPostPagination(req)
+        let foundUsers = await userService.findUsers(queryFilter)
+
+        res.status(STATUSES_HTTP.OK_200)
+            .json(foundUsers)
+
+    },
+
+    async createUser(req: Request, res: Response) {
+        let createdUser = await userService.createUser(req.body.login, req.body.password, req.body.email)
+
+        res.status(STATUSES_HTTP.CREATED_201)
+            .json(createdUser)
+    },
+
+    async deleteUser(req: Request, res: Response) {
+        let deletionStatus: boolean = await userService.deleteUser(req.params.id)
+        if (deletionStatus) {
+            res.sendStatus(STATUSES_HTTP.NO_CONTENT_204)
+        } else {
+            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
+        }
+
+    }
+}
--- src/routes/users-router.ts
+++ src/routes/users-router.ts
@@ -1,24 +1,14 @@
-import {Request, Response, Router} from "express";
-import {queryBlogPostPagination} from "../models/FilterModel";
-import {STATUSES_HTTP} from "./http-statuses-const";
-import {userService} from "../domain/user-service";
-import {UsersWithPaginationModel} from "../models/UsersWithPaginationModel";
+import {Router} from "express";
 import {authenticationCheck} from "../middlewares/auth-mw";
 import {emailValidation, loginValidation, passwordValidation} from "../middlewares/uservalidation-mw";
 import {inputValidationMw} from "../middlewares/inputErrorsCheck-mw";
+import {usersController} from "../controller/users-controller";
 
 export const usersRouter = Router({})
 
 usersRouter.get('/',
     authenticationCheck,
-    async (req: Request, res: Response<UsersWithPaginationModel>) => {
-    let queryFilter = queryBlogPostPagination(req)
-    let foundUsers = await userService.findUsers(queryFilter)
-
-    res.status(STATUSES_HTTP.OK_200)
-        .json(foundUsers)
-
-})
+    usersController.findAllUsers)
 
 usersRouter.post('/',
     authenticationCheck,
@@ -26,21 +16,8 @@ usersRouter.post('/',
     passwordValidation,
     emailValidation,
     inputValidationMw,
-    async (req, res) => {
-        let createdUser = await userService.createUser(req.body.login, req.body.password, req.body.email)
-
-        res.status(STATUSES_HTTP.CREATED_201)
-            .json(createdUser)
-    })
+    usersController.createUser)
 
 usersRouter.delete('/:id',
     authenticationCheck,
-    async (req, res) => {
-    let deletionStatus: boolean = await userService.deleteUser(req.params.id)
-        if (deletionStatus) {
-            res.sendStatus(STATUSES_HTTP.NO_CONTENT_204)
-        } else {
-            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
-        }
-
-    })
+    usersController.deleteUser )
