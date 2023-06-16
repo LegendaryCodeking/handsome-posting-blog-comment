f687cf437d579be77a67edcc642801e4f4bba357
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-16 05:30:16 +0300

    auth-controller main commit

---
+++ src/controller/auth-controller.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+import {UserViewModel} from "../models/UserViewModel";
+import {userService} from "../domain/user-service";
+import {jwtService} from "../application/jwt-service";
+import {STATUSES_HTTP} from "../routes/http-statuses-const";
+
+export const authController = {
+    async loginUser(req: Request, res: Response) {
+    const user: UserViewModel | null = await userService
+        .checkCredentials(req.body.loginOrEmail, req.body.password)
+    if (user) {
+        const token = await jwtService.createJWT(user)
+        res.status(200).json({"accessToken": token})
+        return;
+    }
+    res.sendStatus(STATUSES_HTTP.UNAUTHORIZED_401);
+}
+
+}
--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,18 +1,7 @@
-import {Request, Response, Router} from "express";
-import {userService} from "../domain/user-service";
-import {STATUSES_HTTP} from "./http-statuses-const";
-import {jwtService} from "../application/jwt-service";
-import {UserViewModel} from "../models/UserViewModel";
+import {Router} from "express";
+import {authController} from "../controller/auth-controller";
 
 export const authRouter = Router({})
 
-authRouter.post('/login', async (req: Request, res: Response) => {
-    const user: UserViewModel | null = await userService.checkCredentials(req.body.loginOrEmail, req.body.password)
-    if (user) {
-        const token = await jwtService.createJWT(user)
-        res.status(200).json({"accessToken": token})
-        return;
-    }
-    res.sendStatus(STATUSES_HTTP.UNAUTHORIZED_401);
-})
+authRouter.post('/login', authController.loginUser)
 
