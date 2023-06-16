397e18283f34ce916995cf908b425cd52cd3a717
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-16 05:23:25 +0300

    comments-controller main commit

---
+++ src/controller/comments-controller.ts
@@ -0,0 +1,53 @@
+import {
+    CommentViewModel,
+} from "../models/CommentModel";
+import {Request, Response} from "express";
+import {commentService} from "../domain/comment-service";
+import {STATUSES_HTTP} from "../routes/http-statuses-const";
+
+
+export const commentsController = {
+    async findCommentById(req: Request, res: Response) {
+        let foundComment: CommentViewModel | null = await commentService.findCommentById(req.params.id)
+        if (!foundComment) {
+            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
+            return;
+        } else {
+            res.json(foundComment)
+        }
+    },
+
+    async updateComment(req: Request, res: Response) {
+        let foundComment: CommentViewModel | null = await commentService.findCommentById(req.params.id)
+        if (!foundComment) {
+            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
+            return;
+        }
+
+        if (foundComment.commentatorInfo.userId !== req.user!.id) {
+            res.sendStatus(STATUSES_HTTP.FORBIDDEN_403)
+            return;
+        }
+
+        let updateStatus: boolean = await commentService.updateComment(req.params.id, req.body.content)
+        res.sendStatus(STATUSES_HTTP.NO_CONTENT_204)
+    },
+
+    async deleteComment(req: Request, res: Response) {
+
+        let foundComment: CommentViewModel | null = await commentService.findCommentById(req.params.id)
+        if (!foundComment) {
+            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
+            return;
+        }
+
+        if (foundComment.commentatorInfo.userId !== req.user!.id) {
+            res.sendStatus(STATUSES_HTTP.FORBIDDEN_403)
+            return;
+        }
+
+        let deletionStatus: boolean = await commentService.deleteComment(req.params.id)
+        res.sendStatus(STATUSES_HTTP.NO_CONTENT_204)
+    }
+
+}
--- src/routes/comments-router.ts
+++ src/routes/comments-router.ts
@@ -1,64 +1,25 @@
-import {Request, Response, Router} from "express";
+import {Router} from "express";
 import {authenticationCheckBearer} from "../middlewares/auth-mw";
-import {STATUSES_HTTP} from "./http-statuses-const";
-import {commentService} from "../domain/comment-service";
 import {contentValidation} from "../middlewares/comments-validation-mw";
 import {inputValidationMw} from "../middlewares/inputErrorsCheck-mw";
-import {CommentViewModel} from "../models/CommentModel";
+import {commentsController} from "../controller/comments-controller";
 
 export const commentsRouter = Router({})
 
 
 commentsRouter.get('/:id',
-    async (req: Request, res: Response) => {
-        let foundComment: CommentViewModel | null = await commentService.findCommentById(req.params.id)
-        if (!foundComment) {
-            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
-            return;
-        } else {
-            res.json(foundComment)
-        }
-    }
+    commentsController.findCommentById
 )
 
 commentsRouter.put('/:id',
     authenticationCheckBearer,
     contentValidation,
     inputValidationMw,
-    async (req: Request, res: Response) => {
-        let foundComment: CommentViewModel | null = await commentService.findCommentById(req.params.id)
-        if (!foundComment) {
-            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
-            return;
-        }
-
-        if(foundComment.commentatorInfo.userId !== req.user!.id) {
-            res.sendStatus(STATUSES_HTTP.FORBIDDEN_403)
-            return;
-        }
-
-        let updateStatus: boolean = await commentService.updateComment(req.params.id, req.body.content)
-        res.sendStatus(STATUSES_HTTP.NO_CONTENT_204)
-    }
+    commentsController.updateComment
 )
 
 commentsRouter.delete('/:id',
     authenticationCheckBearer,
     inputValidationMw,
-    async (req: Request, res: Response) => {
-
-        let foundComment: CommentViewModel | null = await commentService.findCommentById(req.params.id)
-        if (!foundComment) {
-            res.sendStatus(STATUSES_HTTP.NOT_FOUND_404)
-            return;
-        }
-
-        if(foundComment.commentatorInfo.userId !== req.user!.id) {
-            res.sendStatus(STATUSES_HTTP.FORBIDDEN_403)
-            return;
-        }
-
-        let deletionStatus: boolean = await commentService.deleteComment(req.params.id)
-        res.sendStatus(STATUSES_HTTP.NO_CONTENT_204)
-    }
+    commentsController.deleteComment
 )
