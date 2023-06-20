9a2c6f77fa14b1dd90e793f53bb320dc01d529f0
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-20 20:19:00 +0300

    Fixed typo

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -4,7 +4,7 @@ import {authenticationCheckBearer} from "../middlewares/auth-mw";
 
 export const authRouter = Router({})
 
-authRouter.post('/login2dfdc', authController.loginUser)
+authRouter.post('/login2', authController.loginUser)
 
 authRouter.get('/me',
     authenticationCheckBearer,
