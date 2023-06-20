3b58bedb8a8901e3c22410d3ac7f3e36e630aab6
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-20 20:22:10 +0300

    Added new endpoint in auth-router

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -6,8 +6,13 @@ export const authRouter = Router({})
 
 authRouter.post('/login', authController.loginUser)
 
+authRouter.post('/registration-confirmation', authController.loginUser)
+
+authRouter.post('/registration', authController.loginUser)
+
+authRouter.post('/registration-email-resending', authController.loginUser)
+
+
 authRouter.get('/me',
     authenticationCheckBearer,
     authController.getInfoAboutMyself)
-
-//Test
