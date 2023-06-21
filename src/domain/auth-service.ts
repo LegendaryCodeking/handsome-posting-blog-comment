4afb3c4a723a4f23ebdc713064ac79ec406d0b1f
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-06-21 03:37:00 +0300

    Fixed error in logic confirmEmail - changed operator in dates comparison

--- src/domain/auth-service.ts
+++ src/domain/auth-service.ts
@@ -10,7 +10,7 @@ export const authService = {
         if (!user) return false
         if (user.emailConfirmation.isConfirmed) return false
         if (user.emailConfirmation.confirmationCode !== code) return false
-        if (new Date(user.emailConfirmation.expirationDate) > new Date()) return false
+        if (new Date(user.emailConfirmation.expirationDate) < new Date()) return false
 
         return await usersRepo.updateConfirmation(user.id)
 
