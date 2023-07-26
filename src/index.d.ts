33453a7dd5f9cd10d95cf8417c4037a10711d091
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-07-26 23:56:54 +0300

    verifyRefreshToken refactor

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 import {UserViewModel} from "./models/Users/UserModel";
 
-
+// Расширяем Request чтобы в нем было свойство user
 declare global {
     declare namespace Express {
         export interface Request {
