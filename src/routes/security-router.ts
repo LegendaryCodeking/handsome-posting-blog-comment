58127c0129f62a496df8b6fdbcf5a0b564331be1
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-07-28 23:58:28 +0300

    securityDevice router main draft

--- src/routes/security-router.ts
+++ src/routes/security-router.ts
@@ -1,3 +1,16 @@
 import {Router} from "express";
 
+
 export const securityRouter = Router({})
+
+securityRouter.get('/',
+    securityController.findAllSessions
+)
+
+securityRouter.delete('/',
+    securityController.terminateAllSessions
+)
+
+securityRouter.delete('/:deviceId',
+    securityController.terminateDeviceSessions
+)
