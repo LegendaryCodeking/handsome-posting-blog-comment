80c4cf08ab5a52a1d2a2b5766dec0f8cc7509865
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-08-13 05:15:52 +0300

    ADD JWT RT MW Usage for Devices TEST

--- src/routes/security-router.ts
+++ src/routes/security-router.ts
@@ -1,17 +1,21 @@
 import {Router} from "express";
 import {securityController} from "../controller/security-controller";
+import {verifyRefreshToken} from "../middlewares/auth-mw";
 
 
 export const securityRouter = Router({})
 
 securityRouter.get('/',
+    verifyRefreshToken,
     securityController.findAllSessions
 )
 
 securityRouter.delete('/',
+    verifyRefreshToken,
     securityController.terminateAllSessions
 )
 
 securityRouter.delete('/:deviceId',
+    verifyRefreshToken,
     securityController.terminateDeviceSessions
 )
