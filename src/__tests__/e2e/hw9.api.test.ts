13fb3722660e8d11f4e6ce03ec43e44d0d3bb8ed
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-08-17 00:52:20 +0300

    Added test for HW 9

---
+++ src/__tests__/e2e/hw9.api.test.ts
@@ -0,0 +1,80 @@
+import request from 'supertest'
+
+import {STATUSES_HTTP} from "../../enum/http-statuses";
+import {app} from "../../app_settings";
+import {Response} from "supertest";
+
+describe('testing ip restriction for registration', () => {
+    beforeAll(async () => {
+        await request(app).delete('/testing/all-data')
+    });
+
+    it("should return 429 error ", async () => {
+
+        const res1: Response = await request(app)
+            .post(`/auth/registration`)
+            .set('Content-Type', 'application/json')
+            .send({
+                login: "User16666",
+                password: "User16666",
+                email: "User1@mail.ru"
+            })
+            .expect(STATUSES_HTTP.NO_CONTENT_204);
+
+        const res2: Response = await request(app)
+            .post(`/auth/registration`)
+            .set('Content-Type', 'application/json')
+            .send({
+                login: "User2666",
+                password: "User2666",
+                email: "User2@mail.ru"
+            })
+            .expect(STATUSES_HTTP.NO_CONTENT_204);
+
+        const res3: Response = await request(app)
+            .post(`/auth/registration`)
+            .set('Content-Type', 'application/json')
+            .send({
+                login: "User3666",
+                password: "User3666",
+                email: "User3@mail.ru"
+            })
+            .expect(STATUSES_HTTP.NO_CONTENT_204);
+
+        const res4: Response = await request(app)
+            .post(`/auth/registration`)
+            .set('Content-Type', 'application/json')
+            .send({
+                login: "User4666",
+                password: "User4666",
+                email: "User4@mail.ru"
+            })
+            .expect(STATUSES_HTTP.NO_CONTENT_204);
+
+        const res5: Response = await request(app)
+            .post(`/auth/registration`)
+            .set('Content-Type', 'application/json')
+            .send({
+                login: "User5666",
+                password: "User5666",
+                email: "User5@mail.ru"
+            })
+            .expect(STATUSES_HTTP.NO_CONTENT_204);
+
+        const res6: Response = await request(app)
+            .post(`/auth/registration`)
+            .set('Content-Type', 'application/json')
+            .send({
+                login: "User6666",
+                password: "User6666",
+                email: "User6@mail.ru"
+            })
+            .expect(STATUSES_HTTP.TOO_MANY_REQUESTS_429);
+
+    }, 50000)
+
+
+
+
+})
+
