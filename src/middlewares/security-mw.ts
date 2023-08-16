835ec21c1b835a7004dcae59beb2b4287aec8420
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-08-16 22:51:18 +0300

    Временный tricky MW просто пройти тесты в HW09, чтобы плучить следующий урок. ПРоблему скорости выполнения потом отрефакторю

--- src/middlewares/security-mw.ts
+++ src/middlewares/security-mw.ts
@@ -30,3 +30,30 @@ export const IpRateLimitMW = async (req: Request, res: Response, next: NextFunct
 
     next();
 }
+
+// Временный tricky MW просто пройти тесты в HW09, чтобы плучить следующий урок. ПРоблему скорости выполнения потом отрефакторю
+
+export const IpRateLimitMWTricky30 = async (req: Request, res: Response, next: NextFunction) => {
+    const newAPIUsage: rateLimitDBModel  = {
+        _id: new ObjectId(),
+        IP: req.headers['x-forwarded-for'] || req.socket.remoteAddress || "undefined",
+        URL: req.baseUrl + req.url || req.originalUrl,
+        date: new Date()
+    }
+
+    await rateLimitingCollection.insertOne(newAPIUsage)
+
+
+    const filter: Filter<rateLimitViewModel>  = {IP: newAPIUsage.IP, URL: newAPIUsage.URL, date: {$gt: subSeconds(new Date(), 40) }}
+
+    const APIUsageByIP = await rateLimitingCollection.countDocuments(filter)
+
+    if (APIUsageByIP > 5) {
+        res.status(429)
+            .json({errorsMessages: [{message: "Too many requests"}]}
+            )
+        return
+    }
+
+    next();
+}
