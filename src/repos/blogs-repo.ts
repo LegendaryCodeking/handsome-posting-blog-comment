c8bfc0581cc98bdaf5ceddb0198dcf7ade6fb613
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-09-29 17:49:17 +0300

    BLOGSREPO. rewrite interaction with DB from using native Mongo client to mongoose models.

--- src/repos/blogs-repo.ts
+++ src/repos/blogs-repo.ts
@@ -1,26 +1,59 @@
 import {BlogDbModel} from "../models/BLogs/BlogModel";
-import {BlogModel} from "../db/db";
+import {BlogModelClass} from "../db/db";
 import {getBlogViewModel} from "../helpers/map-BlogViewModel";
 
 export const blogsRepo = {
     async deleteBlog(id: string): Promise<boolean> {
-        const result = await BlogModel.deleteOne({"id": id});
-        return result.deletedCount === 1
+        // Mongo native driver code
+        // const result = await BlogModelClass.deleteOne({"id": id});
+        // return result.deletedCount === 1
+
+        const blogInstance = await BlogModelClass.findOne({"id": id})
+        if (!blogInstance) return false
+
+        await blogInstance.deleteOne()
+
+        return true
+
     },
     async createBlog(createdBlog: BlogDbModel): Promise<BlogDbModel> {
-        await BlogModel.insertMany([createdBlog])
+        // Mongo native driver code
+        // await BlogModel.insertMany([createdBlog])
+        const blogInstance = new BlogModelClass()
+
+        blogInstance.id = createdBlog.id
+        blogInstance.name = createdBlog.name
+        blogInstance.description = createdBlog.description
+        blogInstance.websiteUrl = createdBlog.websiteUrl
+        blogInstance.createdAt = createdBlog.createdAt
+        blogInstance.isMembership = createdBlog.isMembership
+
+        await blogInstance.save() // await т.к. сохранение асинхронное
 
         return getBlogViewModel(createdBlog)
     },
     async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
-        const result = await BlogModel.updateOne({"id": id}, {
-            $set: {
-                "name": name,
-                "description": description,
-                "websiteUrl": websiteUrl
-            }
-        })
-        return result.matchedCount === 1
+        // Mongo native driver code
+        // const result = await BlogModelClass.updateOne({"id": id}, {
+        //     $set: {
+        //         "name": name,
+        //         "description": description,
+        //         "websiteUrl": websiteUrl
+        //     }
+        // })
+
+        const blogInstance = await BlogModelClass.findOne({"id": id})
+        if (!blogInstance) return false
+
+        blogInstance.name = name
+        blogInstance.description = description
+        blogInstance.websiteUrl = websiteUrl
+
+        await blogInstance.save()
+        return true
+
+        // Mongo native driver code
+        // return result.matchedCount === 1
     },
 
 }
