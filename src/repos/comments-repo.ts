506fb0b4c33c7e2d12b3746ee808a26fe4aa8edf
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-09-29 18:05:28 +0300

    CommentsREPO. rewrite interaction with DB from using native Mongo client to mongoose models.

--- src/repos/comments-repo.ts
+++ src/repos/comments-repo.ts
@@ -8,19 +8,52 @@ import {getCommentViewModel} from "../helpers/map-CommentViewModel";
 
 export const commentsRepo = {
     async updateComment(id: string, content: string): Promise<boolean> {
-        let result = await CommentModelClass.updateOne({"id": id}, {
-            $set: {
-                "content": content
-            }
-        })
-        return result.matchedCount === 1
+        // Mongo native driver code
+        // let result = await CommentModelClass.updateOne({"id": id}, {
+        //     $set: {
+        //         "content": content
+        //     }
+        // })
+        // return result.matchedCount === 1
+
+        const commentInstance = await CommentModelClass.findOne({"id": id})
+        if (!commentInstance) return false
+
+        commentInstance.content = content
+
+        await commentInstance.save()
+
+        return true
+
     },
     async deleteComment(id: string): Promise<boolean> {
-        let result = await CommentModelClass.deleteOne({"id": id})
-        return result.deletedCount === 1
+
+        // Mongo native driver code
+        // let result = await CommentModelClass.deleteOne({"id": id})
+        // return result.deletedCount === 1
+
+        const commentInstance = await CommentModelClass.findOne({"id": id})
+        if (!commentInstance) return false
+
+        await commentInstance.deleteOne()
+
+        return true
+
     },
     async createComment(newComment: CommentDbModel ): Promise<CommentViewModel> {
-        await CommentModelClass.insertMany([newComment])
+        // Mongo native driver code
+        // await CommentModelClass.insertMany([newComment])
+
+        const commentInstance = new CommentModelClass()
+
+        commentInstance.id = newComment.id
+        commentInstance.postId = newComment.postId
+        commentInstance.content = newComment.content
+        commentInstance.commentatorInfo = newComment.commentatorInfo
+        commentInstance.createdAt = newComment.createdAt
+
+        await commentInstance.save();
+
         return getCommentViewModel(newComment);
     }
 }
