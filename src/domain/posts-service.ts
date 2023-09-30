76a43e30c58bf22f775da2f5e2bf36a9398642ff
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-09-30 03:36:40 +0300

    Added class for post's creation

--- src/domain/posts-service.ts
+++ src/domain/posts-service.ts
@@ -1,26 +1,28 @@
 import {postsRepo} from "../repos/posts-repo";
 import {PostDBModel} from "../models/Posts/PostDBModel";
+import {blogsQueryRepo} from "../repos/query-repos/blogs-query-repo";
 
 export const postsService = {
     async deletePost(id: string): Promise<boolean> {
         return postsRepo.deletePost(id)
     },
     async createPost(title: string, shortDescription: string, content: string, blogId: string): Promise<PostDBModel> {
-        const createdPost: PostDBModel = {
-            "id": (+(new Date())).toString(),
-            "title": title,
-            "shortDescription": shortDescription,
-            "content": content,
-            "blogId": blogId,
-            "blogName": "BlogName",
-            "createdAt": new Date().toISOString(),
-            "comments": []
+        const blogName = await blogsQueryRepo.findBlogById(blogId)
 
-        };
+        const createdPost = new PostDBModel(
+            (+(new Date())).toString(),
+            title,
+            shortDescription,
+            content,
+            blogId,
+            blogName!.name,
+            new Date().toISOString(),
+            []
+        )
 
         return await postsRepo.createPost(createdPost)
     },
     async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean> {
-        return postsRepo.updatePost(id,title,shortDescription,content,blogId)
+        return postsRepo.updatePost(id, title, shortDescription, content, blogId)
     }
 }
--- src/models/Posts/PostDBModel.ts
+++ src/models/Posts/PostDBModel.ts
@@ -1,12 +1,26 @@
 import {CommentDbModel} from "../Comments/CommentModel";
 
-export type PostDBModel = {
-    "id": string,
-    "title": string,
-    "shortDescription": string,
-    "content": string,
-    "blogId": string,
-    "blogName": string,
-    "createdAt": string
-    "comments": Array<CommentDbModel>
+export class PostDBModel {
+    constructor(
+        public id: string,
+        public title: string,
+        public shortDescription: string,
+        public content: string,
+        public blogId: string,
+        public blogName: string,
+        public createdAt: string,
+        public comments: Array<CommentDbModel>
+    ) {
+    }
 }
+//
+// export type PostDBModel = {
+//     "id": string,
+//     "title": string,
+//     "shortDescription": string,
+//     "content": string,
+//     "blogId": string,
+//     "blogName": string,
+//     "createdAt": string,
+//     "comments": Array<CommentDbModel>
+// }
